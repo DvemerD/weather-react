bd394f06b49d0589448e4c8f826f477f11245a25
Danil_Rybkin <danil.rybkin.19@gmail.com>
2022-01-09 15:31:38 +0300

    Fixed bug in burger menu

--- src/components/burgerMenu/BurgerMenu.js
+++ src/components/burgerMenu/BurgerMenu.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
@@ -14,28 +15,38 @@ const BurgerMenu = () => {
     const overlayClass = classNames("overlay", { "overlay-active": menuActive }),
         menuClass = classNames("menu", { "menu-active": menuActive });
 
+    const onMenuActive = () => {
+        dispatch(menuChangeActive());
+    }
+
     return (
         <>
             <div className={overlayClass}
-                 onClick={(e) => { return e.target.className.indexOf('overlay', 1) > 0 ? dispatch(menuChangeActive()) : null }}>
+                onClick={(e) => { return e.target.className.indexOf('overlay', 1) > 0 ? onMenuActive() : null }}>
             </div>
             <div className={menuClass}>
                 <div className="menu__wrapper">
                     <div className="menu__header">
                         <div className="menu__title">WELCOME BACK
                             <span className="menu__close"
-                                onClick={() => dispatch(menuChangeActive())}>&#10006;</span>
+                                onClick={() => onMenuActive()}>&#10006;</span>
                         </div>
                         <div className="menu__user">
                             <img src={userImg} alt="user thumbnail" className="menu__thumbnail" />
                             <div className="menu__mail">example@gmail.com</div>
                         </div>
                     </div>
-                    <ul className="menu__list">
-                        <li className="menu__list-item">Home</li>
-                        <li className="menu__list-item">Add City</li>
-                        <li className="menu__list-item">Logout</li>
-                    </ul>
+                    <div className="menu__list">
+                        <Link to="/"
+                              className="menu__list-item"  
+                              onClick={() => onMenuActive()}>Home</Link>
+                        <Link to="/search-city"
+                              className="menu__list-item"  
+                              onClick={() => onMenuActive()}>Add City</Link>
+                        <Link to="/search-city"
+                              className="menu__list-item"  
+                              onClick={() => onMenuActive()}>Logout</Link>
+                    </div>
                 </div>
             </div>
         </>
